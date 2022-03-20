0650f721c29a52a879f39bcadf1659860c80a13c
Graham Paasch <grahampaasch@gmail.com>
2022-03-19 21:16:24 -0500

    all finished! don't need to trim out old events, it's easy enough to just scroll down to the current time

--- austin_events_today.py
+++ austin_events_today.py
@@ -1,10 +1,11 @@
 from lxml import html
+from datetime import datetime
 import requests
 
 page_number = 0
 data = []
 print("Gathering data!")
-while page_number < 100:
+while page_number < 5:
     # Get the page HTML
     page_number += 1
     print(f"Gathered from page {page_number}!")
@@ -22,37 +23,40 @@ while page_number < 100:
     # Give back data in a list of lists
     data.extend(mapped_events_and_times)
 
+# Convert 12 hour time to 24 hour time
+final_answers = []
+for i in data:
+    time12 = i[0]
+    time24 = datetime.strptime(time12, "%I:%M%p")
+    out_time = datetime.strftime(time24, "%H:%M")
+    final_answers.append([out_time, i[1]])
+
 # Sort the data by start time
 def sorting_key(list_element):
     return int(list_element[0].replace(":","").replace("AM","").replace("PM",""))
 
-'''
-    # Clean up and sort the start times
-    start_times = [start_time.replace("\n","").strip().split(' ')[0].upper() for start_time in start_times]
+final_answers.sort(key=sorting_key)
 
-    start_times.sort(key=numbers_only)
-    am_times = [time for time in start_times if "AM" in time]
-    pm_times = [time for time in start_times if "PM" in time]
-    times = am_times + pm_times
-    #
-    for time, event in zip(times, events):
-        link = f"https://do512.com{event.get('data-permalink')}"
-        print(f"{time} | {link}")
-'''
-'''
-event_times = [event.replace("\n","").strip().split(' ')[0].upper() for event in events]
+for final_answer in final_answers:
+    if '00:' not in final_answer[0]:
+        if '01:' not in final_answer[0]:
+            time24 = final_answer[0]
+            time12 = datetime.strptime(time24, "%H:%M")
+            out_time = datetime.strftime(time12, "%I:%M %p")
+            print([out_time, final_answer[1]])
+        else:
+            continue
+    else:
+        continue
 
-def numbers_only(time):
-    return int(time.replace(":","").replace("AM","").replace("PM",""))
-
-event_times.sort(key=numbers_only)
-
-am_events = [time for time in event_times if "AM" in time]
-pm_events = [time for time in event_times if "PM" in time]
-
-link = ""
-for event in am_events:
-    print(f"{event} | {link}")
-for event in pm_events:
-    print(f"{event} | {link}")
-'''
+for final_answer in final_answers:
+    if '00:' in final_answer[0]:
+        time24 = final_answer[0]
+        time12 = datetime.strptime(time24, "%H:%M")
+        out_time = datetime.strftime(time12, "%I:%M %p")
+        print([out_time, final_answer[1]])
+    if '01:' in final_answer[0]:
+        time24 = final_answer[0]
+        time12 = datetime.strptime(time24, "%H:%M")
+        out_time = datetime.strftime(time12, "%I:%M %p")
+        print([out_time, final_answer[1]])
